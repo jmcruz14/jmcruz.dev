233c96812beb1cf7217e86c2baf7cb3c3daf3682
Juan Carlos Cruz <jmcruz14@up.edu.ph>
2023-04-12 10:02:33 +0800

    github pages overhaul

---
+++ main.js
@@ -0,0 +1,28 @@
+// this is a jquery code ... lol
+// $(".nav__link")
+//     .on("mouseover", function() {
+//         $(".nav__link")
+//             .css("text-decoration", underline);
+
+//         // $(this)
+//         //     .css("opacity", 1.0)
+//         //     .css("transform", "scale(1.05)")
+//     })
+//     .on("mouseout", function() {
+//         $(".nav__link")
+//             .css("opacity", 0.8)
+//             .css("transform", "scale(1.0)")
+
+//     });
+
+// var currentPage = window.location.href;
+
+// var links = document.getElementsByClassName('nav__link');
+
+// for (var i = 0; i < links.length; i++) {
+//   var link = links[i];
+  
+//   if (link.href === currentPage) {
+//     link.style.textDecoration = 'underline';
+//   }
+// }
