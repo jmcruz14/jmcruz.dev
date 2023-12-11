c5daf7ca4c1958aacdf348274785283e32aacd95
Jay <jccruz009@yahoo.com>
2023-12-11 12:06:12 +0800

    ✨ feat(PersonalHeader.vue): add new PersonalHeader component The new PersonalHeader component includes the name and profession of the user, as well as links to their LinkedIn, GitHub, and email. This component provides a personal touch to the application and allows users to easily connect with the author.

---
+++ src/components/PersonalHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <hgroup>
+    <h1>
+      Jay Cruz
+    </h1>
+    <h3>
+      Software Developer
+    </h3>
+  </hgroup>
+
+  <nav class="personal-links">
+    <ul>
+      <li>
+        <a title="LinkedIn" href="https://www.linkedin.com/in/juan-carlos-c-54530b16b/" target="_blank" rel="author noreferrer noopener">
+          <img src="~/assets/images/linkedin-logo.png" alt="LinkedIn Link" class="icon-size">
+        </a>
+      </li>
+      <li>
+        <a title="GitHub" href="https://github.com/jmcruz14" target="_blank" rel="author noreferrer noopener">
+        <img src="~/assets/images/github_icon.svg" alt="GitHub Link" class="icon-size">
+        </a>
+      </li>
+      <li>
+        <a title="Email" href="mailto:jccruz009@yahoo.com" target="_blank" rel="author noreferrer noopener">
+          <img src="~/assets/images/email_icon.png" alt="Email Link" class="icon-size">
+        </a>
+      </li>
+    </ul>
+  </nav>
+</template>
