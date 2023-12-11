d9e830a9d09f5d6816a8a6e6a0d4e2d61de2194a
Jay <jccruz009@yahoo.com>
2023-12-12 07:30:51 +0800

    chore: preset node server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  preset: 'node-server',
   devtools: { enabled: true },
   ssr: true,
   spaLoadingTemplate: false,
