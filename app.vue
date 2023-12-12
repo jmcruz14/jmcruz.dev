091700d09918e8cc5a954a8aaf7da7321fe6df32
Jay <jccruz009@yahoo.com>
2023-12-12 21:36:36 +0800

    📦 feat(app.vue, package.json): add @vercel/speed-insights package and implement it in app.vue The @vercel/speed-insights package has been added to the project dependencies to provide performance insights for the application. It has been implemented in app.vue to start collecting these insights. This change will help in monitoring and improving the performance of the application.

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup>
+import { SpeedInsights } from '@vercel/speed-insights'
+</script>
+
 <template>
   <div>
+    <SpeedInsights/>
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
