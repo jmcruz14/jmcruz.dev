4e169f2c4066a95f163512a759ee5ca8bb67c055
Jay <jccruz009@yahoo.com>
2024-03-18 15:14:16 +0800

    feat(NavLinks.vue): add analytics by jay link

--- src/components/NavLinks.vue
+++ src/components/NavLinks.vue
@@ -1,86 +1,112 @@
 <template>
-  <nav class="nav-links">
-    <ul>
-      <li>
-        <NuxtLink
-          to="/about-me"
-          rel="next author noreferrer noopener"
-          class="cursor-hover"
-          :style="{ 'textDecoration': ($route.path === '/about-me' || !$route.path) ? 'underline' : 'none' }"
-        >
-          About Me
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink
-          to="/work"
-          exact
-          rel="next author noreferrer noopener"
-          class="cursor-hover"
-          :style="{ 'textDecoration': $route.path === '/work' ? 'underline' : 'none' }"
-        >
-          Work
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink
-          to="/projects"
-          exact
-          rel="next author noreferrer noopener" 
-          class="cursor-hover"
-          :style="{ 'textDecoration': $route.path === '/projects' ? 'underline' : 'none' }"
-        >
-          Projects
-        </NuxtLink> 
-      </li>
-    </ul>
-  </nav>
+    <nav class="nav-links">
+        <ul>
+            <li>
+                <NuxtLink
+                    to="/about-me"
+                    rel="next author noreferrer noopener"
+                    class="cursor-hover"
+                    :style="{
+                        textDecoration:
+                            $route.path === '/about-me' || !$route.path
+                                ? 'underline'
+                                : 'none',
+                    }"
+                >
+                    About Me
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink
+                    to="/work"
+                    exact
+                    rel="next author noreferrer noopener"
+                    class="cursor-hover"
+                    :style="{
+                        textDecoration:
+                            $route.path === '/work' ? 'underline' : 'none',
+                    }"
+                >
+                    Work
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink
+                    to="/projects"
+                    exact
+                    rel="next author noreferrer noopener"
+                    class="cursor-hover"
+                    :style="{
+                        textDecoration:
+                            $route.path === '/projects' ? 'underline' : 'none',
+                    }"
+                >
+                    Projects
+                </NuxtLink>
+            </li>
+            <li>
+                <NuxtLink
+                    to="/analytics-by-jay"
+                    exact
+                    rel="next author noreferrer noopener"
+                    class="cursor-hover"
+                    :style="{
+                        textDecoration:
+                            $route.path === '/analytics-by-jay'
+                                ? 'underline'
+                                : 'none',
+                    }"
+                >
+                    Analytics by Jay
+                </NuxtLink>
+            </li>
+        </ul>
+    </nav>
 </template>
 
 <script>
-import { ref, onMounted } from 'vue';
-import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router';
+import { ref, onMounted } from "vue";
+import { useRoute, useRouter, onBeforeRouteUpdate } from "vue-router";
 
 export default {
-  setup () {
+    setup() {
+        const route = useRoute();
+        const router = useRouter();
+        const activeLink = ref(null);
 
-    const route = useRoute();
-    const router = useRouter();
-    const activeLink = ref(null);
+        onBeforeRouteUpdate((to, from) => {
+            console.warn("navigation-to", to);
+            console.warn("navigation-from", from);
+            console.warn("route-data", route);
+        });
 
-    onBeforeRouteUpdate((to, from) => {
-      console.warn('navigation-to', to);
-      console.warn('navigation-from', from);
-      console.warn('route-data', route);
-    })
-    
-    // Check if the current URL ends with "index.html"
-    // if (currentUrl.endsWith("index.html") || currentUrl.endsWith("/")) {
-    //   // If yes, set the text-decoration of the element with href "index.html" to "underline"
-    //   // since we currently have two html files in the root directory
-    //   // simple if-else statement is fine
-    //   document.querySelector("a[href='index.html']").style.textDecoration = "underline";
-    // } else if (currentUrl.endsWith("work.html")) {
-    //   document.querySelector("a[href='work.html']").style.textDecoration = "underline";
-    // } else if (currentUrl.endsWith("projects.html")) {
-    //   // If not, set the text-decoration of the element with ID "my-link" to "none"
-    //   document.querySelector("a[href='projects.html']").style.textDecoration = "underline";
-    // }
+        // Check if the current URL ends with "index.html"
+        // if (currentUrl.endsWith("index.html") || currentUrl.endsWith("/")) {
+        //   // If yes, set the text-decoration of the element with href "index.html" to "underline"
+        //   // since we currently have two html files in the root directory
+        //   // simple if-else statement is fine
+        //   document.querySelector("a[href='index.html']").style.textDecoration = "underline";
+        // } else if (currentUrl.endsWith("work.html")) {
+        //   document.querySelector("a[href='work.html']").style.textDecoration = "underline";
+        // } else if (currentUrl.endsWith("projects.html")) {
+        //   // If not, set the text-decoration of the element with ID "my-link" to "none"
+        //   document.querySelector("a[href='projects.html']").style.textDecoration = "underline";
+        // }
 
-    return {
-      activeLink,
-    }
-  }
-}
+        return {
+            activeLink,
+        };
+    },
+};
 </script>
 
 <style scoped>
 .active-link {
-  text-decoration: underline;
+    text-decoration: underline;
 }
 
 @media screen and (max-width: 640px) {
-  nav.nav-links {
+    nav.nav-links {
         position: static;
     }
 
@@ -94,9 +120,8 @@ export default {
 }
 
 @media screen and (min-width: 640px) and (max-width: 768px) {
-  nav.nav-links > ul > li > a {
-      font-size: 20px;
-  }
+    nav.nav-links > ul > li > a {
+        font-size: 20px;
+    }
 }
-
 </style>
